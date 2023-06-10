2fc1cc425c4520d5733dc7cad65ff21c041692a7
prashastSINGH <prashast9450@gmail.com>
2023-06-10 23:33:11 +0530

    Signed-off-by: prashastSINGH <prashast9450@gmail.com>

--- helper/database.js
+++ helper/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize  = new Sequelize(process.env.DB_NAME,process.env.DB_LOGIN , process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost',
+    host: process.env.DB_HOST,
 
     dialectOptions: {
         timezone: 'local'
